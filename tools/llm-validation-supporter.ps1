4acb30b443f90c8c9aca9a1a63590b1023bb6893
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-09-04 16:13:49 +0200

    Update prompt in script

--- tools/llm-validation-supporter.ps1
+++ tools/llm-validation-supporter.ps1
@@ -1,14 +1,28 @@
+<#
+	.SYNOPSIS
+		A short powershell script for generating a prompt for an LLM to aid with finding errors in the specification.
+#>
+
+
 param (
 	[String]
-	$TokenFileName = "tokens.txt"
+	$PromptFileName = "prompt.txt"
 )
 
 
-"Please consider the following spec.md file and the following json schemas and try to find deviations between the written specification and the json schema." | Out-File -FilePath $TokenFileName
-"`n-----> file spec.md:`n" | Out-File -FilePath $TokenFileName -Append
-Get-Content "$PSScriptRoot/../spec.md" | Out-File -Append -FilePath $TokenFileName
+"
+---> INSTRUCTIONS:
+
+Listed below are a technical standard document called spec.md and a list of JSON schema files.
+Try to find inconsistencies between the data structures outlined in spec.md and the JSON schema files.
+Make a list of such contradictions and give the exact section in spec.md and the name of the schema which are in contradiction.
+
+" | Out-File -FilePath $PromptFileName
+
+"`n---> file spec.md:`n" | Out-File -FilePath $PromptFileName -Append
+Get-Content "$PSScriptRoot/../spec.md" | Out-File -Append -FilePath $PromptFileName
 
 Get-ChildItem -Path "$PSScriptRoot/../json-schema/" -Filter *.json -File -Recurse | ForEach-Object {
-	"`n-----> file $($_.Name):`n" | Out-File -FilePath $TokenFileName -Append
-	Get-Content $_.FullName | Out-File -Append -FilePath $TokenFileName
+	"`n---> file $($_.Name):`n" | Out-File -FilePath $PromptFileName -Append
+	Get-Content $_.FullName | Out-File -Append -FilePath $PromptFileName
 }
